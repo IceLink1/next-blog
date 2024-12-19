54b56adf424819197c4a82a5ebfbdf907d51c8cf
ICELINK <icelink39@gmail.com>
2024-12-19 06:38:09 +0500

    Update TheHeader.tsx

--- components/TheHeader.tsx
+++ components/TheHeader.tsx
@@ -4,7 +4,7 @@ const TheHeader = () => {
   return (
     <header>
       <Link href="/">Home</Link>
-      <Link href="/blog">Blog</Link>
+      <Link href="/blog">Blogs</Link>
       <Link href="/about">About</Link>
     </header>
   );
